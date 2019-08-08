3bd1009aae73bbc5024a2262d96fbfa0710efa68
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-08-08 16:43:17 +0200

    conect API with axios

--- src/pages/CardsList.js
+++ src/pages/CardsList.js
@@ -3,9 +3,16 @@ import axios from 'axios';
 
 class CardsList extends Component {
 
+  state = {
+    cards: [],
+  }
+
   componentDidMount() {
-    axios.get('url')
-      .then(() => {})
+    axios.get('https://api.magicthegathering.io/v1/cards')
+      .then((response) => {
+        console.log(response);
+        
+      })
       .catch((error) => {
         console.log(error);
         
